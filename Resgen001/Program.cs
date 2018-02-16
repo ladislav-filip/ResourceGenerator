dca6d194eeeb83f6380cc531247fa31efb484424
LF <ladislav.filip@fullsys.cz>
2018-02-16 21:50:56 +0100

    Inicializace

--- Resgen001/Program.cs
+++ Resgen001/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Resgen001
 {
-    class Program
+    /// <summary>
+    /// Vygenerované soubory se automaticky přepíšou do tohoto projektu.
+    /// Je tedy možné po dalším spuštění přistupovat přímo na resource, přidané při generování.
+    /// </summary>
+    public class Example
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Console.WriteLine(Fullsys.FIS.Core.Resources.Common.Entity.Ptpvz.Car);
+
+            var dict = new Dictionary<string, string>()
+            {
+                { "Car", "Auto" },
+                { "Plane", "Letadlo" }
+            };
+
+            var dstDir = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine(dstDir);
+            dstDir = dstDir.Substring(0, dstDir.IndexOf("bin"));
+            var resxFile = Path.Combine(dstDir, "Ptpvz.resx");
+            Console.WriteLine(resxFile);
+
+            var generator = new ResGenerator(resxFile, dict);
+            generator.NameSpace = "Fullsys.FIS.Core.Resources.Common.Entity";
+            generator.BaseName = "Ptpvz";
+            generator.ResourceNameSpace = "Resgen001";
+            Console.WriteLine(generator.DesignerFileName);
+            generator.Start();
+
+            Console.WriteLine("Finnish.");
+            Console.ReadKey();
         }
     }
 }
---
+++ Resgen001/ResGenerator.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Resources;
+using System.Resources.Tools;
+
+namespace Resgen001
+{
+    public class ResGenerator
+    {
+        public string ResxFileName { get; }
+
+        public string DesignerFileName
+        {
+            get
+            {
+                var ext = Path.GetExtension(ResxFileName);
+                var result = ResxFileName.Replace(ext, ".Designer.cs");
+                return result;
+            }
+        }
+
+        public string NameSpace { get; set; }
+
+        public string BaseName { get; set; }
+
+        public string ResourceNameSpace { get; set; }
+
+        private readonly IDictionary<string, string> m_dictionary;
+        private readonly string m_fileCsv;
+
+        public ResGenerator(string resxFileName)
+        {
+            ResxFileName = resxFileName;
+        }
+
+        public ResGenerator(string resxFileName, IDictionary<string, string> dictionary) : this(resxFileName)
+        {
+            m_dictionary = dictionary;
+        }
+
+        public ResGenerator(string resxFileName, string fileCsv) : this(resxFileName)
+        {
+            m_fileCsv = fileCsv;
+        }
+
+        public void Start()
+        {
+            CreateResXFile();
+
+            var sw = new StreamWriter(DesignerFileName);
+            var provider = new CSharpCodeProvider();
+            var code = StronglyTypedResourceBuilder.Create(
+                ResxFileName, // resxFile
+                BaseName, // baseName
+                NameSpace, // generatedCodeNamespace
+                ResourceNameSpace, // resourcesNamespace
+                provider, // codeProvider
+                false, // internalClass
+                out string[] errors);
+
+            if (errors.Length > 0)
+                foreach (var error in errors)
+                    Console.WriteLine(error);
+
+            provider.GenerateCodeFromCompileUnit(code, sw, new CodeGeneratorOptions());
+            sw.Close();
+        }
+
+        private void CreateResXFile()
+        {
+            using (var rw = new ResXResourceWriter(ResxFileName))
+            {
+                foreach (var d in m_dictionary)
+                {
+                    rw.AddResource(d.Key, d.Value);
+                }
+                rw.Generate();
+                rw.Close();
+            }
+        }
+    }
+}
